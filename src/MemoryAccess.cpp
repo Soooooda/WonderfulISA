5307d62bc5b11e37a2f3041d408861db3e39cc62
JinnairArthas <tianshuli@umass.edu>
2021-04-06 05:31:31 -0400

    fix the cachebug
    修正了如果cache内部有memory依然需要等3轮的bug

--- src/MemoryAccess.cpp
+++ src/MemoryAccess.cpp
@@ -16,29 +16,39 @@ output *MemoryAccess::execute(Simulator* simulator, Register* registe)
     if (this->time == 0) {
         cout<<"Mem Opt:"<<data->inst.instruction_operator <<endl;
         if (data->inst.instruction_operator == 1) {
-            data->value = simulator->read_memory(data->inst.address);
-            cout<<"Start Load"<<endl;
-            this->time = 3;
+            if(simulator->l1_cache.request_cache(data->inst.address) == -1){
+                data->value = simulator->read_memory(data->inst.address);
+                cout<<"Start Load"<<endl;
+                this->time = 3;
+            }
+            else{
+                data->value = simulator->read_memory(data->inst.address);
+                cout<<"Start Load"<<endl;
+                this->time = 1;
+            }
         } else if (data->inst.instruction_operator == 2)
         {
-            simulator->write_memory(data->inst.address, registe->get(data->inst.operands[0]));
-            cout<<"Start Store"<<endl;
-            this->time = 3;
+            if(simulator->l1_cache.request_cache(data->inst.address) == -1){
+                simulator->write_memory(data->inst.address, registe->get(data->inst.operands[0]));
+                cout<<"Start Store"<<endl;
+                this->time = 3;
+            }
+            else{
+                simulator->write_memory(data->inst.address, registe->get(data->inst.operands[0]));
+                cout<<"Start Store"<<endl;
+                this->time = 1;
+            }
         } else {
             return data;
         }
     }
-
     this->time--;
-    if(this->time == 1) {
-        if(data->inst.instruction_operator == 1){
-            registe->set(data->inst.operands[0],data->value);
-        }
-
-    }
 
     cout<<"mem time"<<this->time<<endl;
     if (this->time == 0) {
+        if(data->inst.instruction_operator == 1){
+            registe->set(data->inst.operands[0],data->value);
+        }
         return data;
     }
     return NULL;
